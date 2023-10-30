7b281ac4987578a4726a1dfed5cc546be71a8b67
Paul Garell <prgarell@gmail.com>
2023-10-30 12:03:17 -0400

    implement win checking

--- gomoku-game/main.py
+++ gomoku-game/main.py
@@ -24,18 +24,86 @@ def make_move(state, turn):
 			state[x][y] = turn
 			validCords = True
 	
-	return state
+	return state, (x, y)
+
+'''
+vary in i direction
+keep j constant 
+'''
+def check_flat(state, i, j):
+	ci = i
+	while ci > 0 and state[ci][j] == state[i][j]: 
+		ci -= 1
+	if state[ci][j] != state[i][j]:
+		left = ci + 1
+	else:
+		left = ci
+	ri = i
+	while ri < (len(state) - 1) and state[ri][j] == state[i][j]: 
+		ri += 1 
+	if state[ri][j] != state[i][j]:
+		right = ri + 1
+	else:
+		right = ri
+	if ri - ci >= 5: 
+		return True 
+	return False
 
 
+def check_diag(state, i, j):
+	li = i
+	lj = j
+	while li > 0 and lj > 0 and state[li][lj] == state[i][j]:
+		li -= 1
+		lj -= 1
+	if state[li][lj] != state[i][j]:
+		lowi = li + 1
+		lowj = lj + 1
+	else:
+		lowi = li
+		lowj = lj
+
+	ui = i
+	uj = j
+	while ui < len(state) - 1 and uj < len(state) - 1 and state[ui][uj] == state[i][j]:
+		ui += 1
+		uj += 1
+	if state[ui][uj] != state[i][j]:
+		upi = ui + 1
+		upj = uj + 1
+	else:
+		upi = ui
+		upj = uj
+
+	if ui - li >= 5: 
+		return True
+	return False
 '''
 input:
 	state: board state (15x15 numpy array)
 output:
 	win: boolean value of whether current player won
 '''
-def check_win():
-	pass
+def check_win(state, last):
+	#check diagonal
+	#check horizontal
+	#check vertical
+	i, j = last
+	vert = check_flat(state, i, j)
+	horiz = check_flat(state.T, j, i)
+	
+	print("hor" + str(horiz))
+	print("vert" + str(vert))
+	
+	#i think this should work??
+	diagl = check_diag(state, i, j)
+	diagr = check_diag(np.fliplr(state), i, len(state) -1 - j)
 
+	diag = diagl or diagr
+
+	print("diag "+str(diag))
+	return (horiz or vert) or diag
+	
 
 def play_game(state):
 	#check if winner
@@ -46,13 +114,19 @@ def play_game(state):
 
 	while gameIsRunning:
 		curMove *= -1 #switch turns
-		state = make_move(state, curMove)
+		state, move = make_move(state, curMove)
 		print(state)
-		gameIsRunning = not check_win()
+		gameIsRunning = not check_win(state,move)
+		if np.sum(np.sum(np.abs(state[:, :]))) == 225:
+			gameIsRunning = False
+	
 		
+	if check_win(state, move):
+		print(str(curMove) + " won!")
+	else:
+		print("board full, no winner.")
 
-
-
+	
 
 if __name__ == "__main__":
 	back = np.zeros((15, 15))
